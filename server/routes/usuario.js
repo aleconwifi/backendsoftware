c2d38bc3fe69fe3fc42a75b68295d551f075e21a
Alejandro Marcano <alejandro.marcano@correo.unimet.edu.ve>
2018-10-05 21:47:23 -0400

    :collision: GET y delete pepa

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -13,9 +13,17 @@ const app = express();
 //prueba 
 app.get('/usuario', function(req, res) {
 
+    //si viene esa variable desde voy a suponer que
+    //quiere desde, sino desde la pagina 1 por defecto
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
 
+    let limite = req.query.limite || 5; //si no te especifica el limite son 5
+    limite = Number(limite);
 
-    Usuario.find({})
+    Usuario.find({}, 'nombre email role estado google img')
+        .skip(desde) //saltarte registros
+        .limit(limite) //paginar a 5 usuarios
         .exec((err, usuarios) => {
 
             if (err) {
@@ -26,11 +34,19 @@ app.get('/usuario', function(req, res) {
                 });
             }
 
-            res.json({
-                ok: true,
-                usuarios
+            Usuario.count({}, (err, conteo) => {
+                //cuenta todos los usuarios
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+
+                });
 
             });
+
+
 
 
         })
@@ -108,9 +124,40 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario', function(req, res) {
+app.delete('/usuario/:id', function(req, res) {
 
-    res.json('delete Usuario panita');
+
+    let id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+
+            }); //bad request es 400
+        };
+        if (usuarioBorrado == null) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+
+        });
+
+
+
+    });
+
+
 });
 
 module.exports = app;
